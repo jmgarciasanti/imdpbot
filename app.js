86e2f9d0c325c8040f6f12c2632b113c010e7954
jmgarciasanti <jmgarciasanti@hotmail.com>
2016-10-12 00:41:00 +0200

    appid & appPassword

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ server.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_N
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: '8bb41ef7-4cab-4bf6-92af-9d80ffb8e9a8', 
-    appSecret: '167e15c91257421ebc79a4d7065de5d7'
+    appPassword: 'vxEbY1YJUXMRyWdczVpRCoj'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
