fe036ae5f630824ce07055dc2c58d75eb87176aa
jmgarciasanti <jmgarciasanti@hotmail.com>
2016-10-14 15:43:05 +0200

    tap en la imagen de rootDialog

--- cardsDialog.js
+++ cardsDialog.js
@@ -11,13 +11,13 @@ module.exports = {
             .textFormat(builder.TextFormat.xml)
             .attachments([
                     new builder.HeroCard(session)
-                    .title("Hero Card")
-                    .subtitle("Space Needle")
-                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                    ])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
+                        .title("Hero Card")
+                        .subtitle("Space Needle")
+                        .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                        .images([
+                            builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                        ])
+                        .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
                 ]);
                 session.send(msg);
                 
--- rootDialog.js
+++ rootDialog.js
@@ -7,11 +7,12 @@ module.exports = {
             function (session) {
                 // Send a greeting and show help.
                 var card = new builder.HeroCard(session)
-            .title("Microsoft Bot Framework")
-            .text("Your bots - wherever your users are talking.")
-            .images([
-                    builder.CardImage.create(session, "http://www.mapfre.es/seguros/images/894x248-general-quienessomos_tcm744-128789.jpg")
-                ]);
+                    .title("Microsoft Bot Framework")
+                    .text("Your bots - wherever your users are talking.")
+                    .images([
+                        builder.CardImage.create(session, "http://www.mapfre.es/seguros/images/894x248-general-quienessomos_tcm744-128789.jpg")
+                    ])
+                    .tap(builder.CardAction.openUrl(session, "http://www.mapfre.es"));
                 var msg = new builder.Message(session).attachments([card]);
                 session.send(msg);
                 session.send("Hi... I'm the Microsoft Bot Framework demo bot for Skype. I can show you everything you can use our Bot Builder SDK to do on Skype.");
