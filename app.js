e19042ae143a7ea3bc72fcdfa1c154f9c32ad2e9
jmgarciasanti <jmgarciasanti@hotmail.com>
2016-10-23 11:13:41 +0200

    Log del webhook

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ server.get(/\/js\/?.*/, restify.serveStatic({
 
 // webhook for Facebook
 function processWebhook(req, res, next) {
+    console.log("Hook received: " + JSON.stringify(req.body));
     res.send({ "success": true });
 }
 server.get('/webhook', processWebhook);
