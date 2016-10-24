04d4c8bcfde5f74a922a97b05738fb039b80a696
jmgarciasanti <jmgarciasanti@hotmail.com>
2016-10-24 20:23:29 +0200

    1ª version de MAFRIUS

--- aboutDialog.js
+++ aboutDialog.js
@@ -7,11 +7,11 @@ module.exports = {
             // Send a greeting and show help.
             var card = new builder.HeroCard(session)
                 .title("AwarenessBot @ MAPFRE - IMDP")
-                .text("Hi " + session.message.user.name + ", I'm a bot created by Verti...let me help you better understand the world of insurance")
+                .text("Hi " + session.message.user.name + ", I'm MAFRIUS a bot created by MAPFRE...let me help you better understand the world of insurance")
                 .images([
-                    builder.CardImage.create(session, "http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/verti.jpg")
+                    builder.CardImage.create(session, "http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/bot logo.jpg")
                 ])
-                .tap(builder.CardAction.openUrl(session, "https://www.verti.es"));
+                .tap(builder.CardAction.openUrl(session, "https://www.mapfre.es"));
             var msg = new builder.Message(session).attachments([card]);
             session.endDialog(msg);
         });
--- carProductsDialog.js
+++ carProductsDialog.js
@@ -50,26 +50,26 @@ module.exports = {
 var products = [
     {
         name: 'Car insurance',
-        description: 'cars and little trucks',
-        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros-coche.png',
-        url: 'https://www.verti.es/seguros/seguros-coche.html'
+        description: 'En MAPFRE te ofrecemos una gran variedad de seguros de coche entre los que puedes elegir dependiendo de tus necesidades. Con los Seguros de Coches Terceros Básico, Terceros Ampliado, Terceros Completo, Todo Riesgo con Franquicia y Todo Riesgo sin Franquicia podrás contratar desde una cobertura más básica, hasta paquetes de garantías más completas.',
+        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros coche.jpg',
+        url: 'https://www.mapfre.es/seguros/particulares/coche/seguros-de-coche/comparativa-seguros-de-coche/'
     },
     {
-        name: 'Motorbike insurance',
-        description: 'only bikes...',
-        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros-moto.png',
-        url: 'https://www.verti.es/seguros/seguros-moto.html'
+        name: 'Car insurance YCAR',
+        description: 'MAPFRE ofrece a los conductores noveles o menores de 30 años seguros para jóvenes YCAR, con mejores condiciones a través de pólizas cuyos precios serán más competitivos dependiendo de los hábitos de conducción del asegurado. ',
+        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros coche ycar.jpg',
+        url: 'https://www.mapfre.es/seguros/particulares/coche/seguros-para-jovenes-ycar/comparativa-seguros-jovenes-ycar/'
     },
     
     {
-        name: 'Odometer insurance',
-        description: 'people who don\'t use the car regularly',
-        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros-cuentakm.png',
-        url: 'https://www.verti.es/seguros/poliza-CuentaKms.html'
+        name: 'Ecologic car insurance',
+        description: 'MAPFRE dispone de seguros para coches ecológicos que incluyen un sistema de pago por uso para vehículos eléctricos, híbridos y ecológicos con menos de 120 gramos de CO2 por Kilómetro y que premia a los buenos conductores.',
+        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros coche ecologicos.jpg',
+        url: 'https://www.mapfre.es/seguros/particulares/coche/seguros-de-coches-ecologicos/comparativa-seguros-de-coches-ecologicos/'
     },
     {
-        name: 'Six wheel insurance',
-        description: 'car and motorbike in a single product',
-        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros-6ruedas.png',
-        url: 'https://www.verti.es/seguros/seguros-6-ruedas.html'
+        name: 'Motorbike insurance',
+        description: 'Los seguros de moto de MAPFRE ofrecen a los conductores de ciclomotores y motocicletas las garantías más completas dependiendo de la amplitud de coberturas que necesite.',
+        img: 'http://imdpbot-jmgarciasanti.rhcloud.com/resources/img/seguros moto.jpg',
+        url: 'https://www.mapfre.es/seguros/particulares/moto/seguros-para-motos/comparativa-seguros-para-motos/'
     }];
